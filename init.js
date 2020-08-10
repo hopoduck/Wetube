de461f84d09c7d93ebf940c71fa7e46bbffeec96
hopoduck <hopoduck>
2020-08-11 00:28:01 +0900

    3.2 end new DB

--- init.js
+++ init.js
@@ -1,11 +1,14 @@
 // git push origin master
 // git pull origin master
 
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handelListening = () =>
-  console.log(`✔ Listening on http://localhost:${PORT}`);
+  console.log(`✅ Listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handelListening);
