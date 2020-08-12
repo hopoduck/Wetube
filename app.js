f3257913142fb74c66b826b941dd07bae889187a
hopoduck <hopoduck>
2020-08-12 21:20:46 +0900

    3.7 UPLOAD VIDEO!

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json()); // 모든 라우터에 적용되는 함수
 app.use(bodyParser.urlencoded({ extended: true })); // 모든 라우터에 적용되는 함수
